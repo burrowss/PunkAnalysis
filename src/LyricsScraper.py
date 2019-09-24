065c24a1e6cdd2eaad43c9f71c2744c6b7e051ea
burrowss <burrowss@allegheny.edu>
2019-09-23 23:10:16 -0400

    Reformatted LyricsScraper. Writes songs to new folder in song.py (in LyricsGenius Package)

--- src/LyricsScraper.py
+++ src/LyricsScraper.py
@@ -5,20 +5,31 @@ with open("AccessToken.txt", "r") as file:
 
 genius = lyricsgenius.Genius(token)
 
-genius.verbose = False  # Turn off status messages
+# Turn off status messages
+genius.verbose = False
+
 # Remove section headers (e.g. [Chorus]) from lyrics when searching
 genius.remove_section_headers = True
+
 # Exclude songs with these words in their title
 genius.excluded_terms = ["(Remix)", "(Live)"]
+
 # Include hits thought to be non-songs (e.g. track lists)
 genius.skip_non_songs = True
 
+""" Search by artist """
+
 artist = genius.search_artist("The Wonder Years", max_songs=3, sort="title")
-print(artist.songs)
+# print(artist.songs)
 
-song = genius.search_song("Cardinals", artist.name)
-print(song.lyrics)
+""" Single Song search """
+# song = genius.search_song("Cardinals", artist.name)
+# print(song.lyrics)
+song = genius.search_song("", artist.name)
 
+""" Adding songs to artist object """
 artist.add_song(song)
 
 artist.save_lyrics()
+
+print(artist.songs)
